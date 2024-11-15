4075054a0fc4c69df7b330ed9d83f7c53bcc2fdd
Forrest Baird <forrestmsbaird@gmail.com>
2024-11-15 09:46:57 -0800

    New setups, unfinished functions

--- fxyz_ml_ele_fctns.py
+++ fxyz_ml_ele_fctns.py
@@ -1,14 +1,43 @@
-
+from fxyz_ml_math_ops import *
 
 def reduce_fraction(num: int, denom: int) -> int:
 
     # Function Steps:
-    # Determine all factors of numerator and denomonator
-    # Go through both arrays and cancel out facotrs
+    #! Determine all factors of numerator and denomonator
+    #! Go through both arrays and cancel out factors
     # Multiply remaining factors together
     # Return an array with reduce numerator and denominator
+
+    # Stage 1: Get Factors
+    num_to_reduce: int = num
+    denom_to_reduce: int = denom
 
     num_factors = []
     denom_factors = []
 
-    # Clean up the code
+    # Stage 2: ???
+
+    # Stage 3: Create Reduced Numbers
+    reduced_num = PROD_INIT
+    reduced_denom = PROD_INIT
+
+    if range(len(num_factors)) == range(len(denom_factors)):
+
+        for i in num_factors:
+
+            reduced_num *= num_factors[i]
+            reduced_denom *= denom_factors[i]
+
+        return [reduced_num, reduced_denom]
+
+    else: 
+
+        for i in num_factors:
+
+            reduced_num *= num_factors[i]
+
+        for i in denom_factors:
+
+            reduced_denom *= denom_factors[i]
+
+        return [reduced_num, reduced_denom]
