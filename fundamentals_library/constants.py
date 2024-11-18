df9ef4e72b79cca50a59cd9adf69ee3df368113b
deepforrest <forrest@fmsbaird.com>
2024-11-17 23:20:27 -0800

    little things happening

--- fundamentals_library/constants.py
+++ fundamentals_library/constants.py
@@ -1,5 +1,4 @@
 from math_library.fund_comp import *
-from fundamentals_library import *
 
 # Part 1: Readibility Constants
 BINARY_BASE: int = 2
--- math_library/algebraic_functions.py
+++ math_library/algebraic_functions.py
@@ -1,3 +1,4 @@
+from math import *
 from math_library.fund_comp import *
 from fundamentals_library.constants import *
 
