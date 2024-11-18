8becde7aac753a7ca714fad397ea8ba06dba84b4
Forrest Baird <forrestmsbaird@gmail.com>
2024-11-17 18:11:30 -0800

    business functions

--- fxyz_ml_biz_fun.py
+++ fxyz_ml_biz_fun.py
@@ -1,13 +1,17 @@
 # Calculates the adjusted hourly wage earned from working with clients based on travel time (an opp. cost)
-def adj_travel_wage(wage: float, work_time_hrs: float, travel_time_hrs: float, travel_charge: float = 0):
 
-    return float_quotient(add(product(wage, work_time_hrs), travel_charge), add(work_time_hrs, product(2, travel_time_hrs)))
+def assignment_earnings(hourly_rate: float, hours_worked: float, travel_charges: float = 0):
 
+    return add(product(hourly_rate, hours_worked), travel_charges)
+
+def work_time(work_hrs: float, travel_hrs: float):
+
+    return add(work_hrs, product(2, travel_hrs))
+
+def adj_travel_wage(hourly_rate: float, work_hrs: float, travel_hrs: float, travel_charge: float = 0):
+
+    return float_quotient(assignment_earnings(hourly_rate, work_hrs, travel_charge), work_time(work_hrs, travel_hrs))
 
 def marketing_roi():
 
     pass
-
-def assignment_earnings(hourly_charges: float, hours_worked: float, travel_charges: float = 0):
-
-    return add(product(hourly_charges, hours_worked), travel_charges)
