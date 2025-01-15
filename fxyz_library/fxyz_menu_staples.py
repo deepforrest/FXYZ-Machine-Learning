41b7417c012fedee8e36d1e22944a40c303ec34c
deepforrest <forrest@fmsbaird.com>
2025-01-15 08:40:52 -0800

    making a main menu!

---
+++ fxyz_library/fxyz_menu_staples.py
@@ -0,0 +1,31 @@
+def fxyz_menu_header() -> None:
+
+    print("The ForrestXYZ 2025\n\n")
+
+def fxyz_welcome_screen() -> None:
+
+    print("Welcome to the ForrestXYZ!\n\n")
+
+def fxyz_division_options() -> None:
+
+        print("Main Menu: Please select from the following options (1-12):\n\n\
+          1: Organization\n\
+          2: Physical\n\
+          3: Mental\n\
+          4: Spiritual\n\
+          5: Chronological\n\
+          6: Financial\n\
+          7: Material\n\
+          8: Knowledge\n\
+          9: Skillsets\n\
+          10: Creative\n\
+          11: Social\n\
+          12: Business\n\n")
+
+def fxyz_make_division_selection() -> None:
+
+    input("Please select your division number to proceed (1-12): ")
+
+def fxyz_menu_footer() -> None:
+
+    print("(C) 2017-2025 Deep Forrest Enterprises.  The ForrestXYZ is part of Deep Forrest Enterprises.\n")
---
+++ fxyz_library/fxyz_test_menu.py
@@ -0,0 +1,14 @@
+# This is to test out the format of a basic menu for the ForrestXYZ.  It will likely be scrapped once a better one is in place
+
+from fxyz_menu_staples import *
+
+def fxyz_main_menu() -> None:
+
+    fxyz_menu_header()
+    fxyz_menu_footer()
+    fxyz_welcome_screen()
+    fxyz_division_options()
+    fxyz_make_division_selection()
+    
+
+fxyz_main_menu()
