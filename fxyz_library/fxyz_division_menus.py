3f8caeef93c94e603cad883f2b05cdcd10637493
Forrest <forrestmsbaird@gmail.com>
2025-01-15 21:10:53 -0800

    Added new division menus

---
+++ fxyz_library/fxyz_division_menus.py
@@ -0,0 +1,66 @@
+def fxyz_select_division(division_number: int) -> None:
+
+    if division_number == 1: fxyz_organization_menu()
+    if division_number == 2: fxyz_physical_menu()
+    if division_number == 3: fxyz_mental_menu()
+    if division_number == 4: fxyz_spiritual_menu()
+    if division_number == 5: fxyz_chronological_menu()
+    if division_number == 6: fxyz_financial_menu()
+    if division_number == 7: fxyz_material_menu()
+    if division_number == 8: fxyz_knowledge_menu()
+    if division_number == 9: fxyz_skillsets_menu()
+    if division_number == 10: fxyz_creative_menu()
+    if division_number == 11: fxyz_social_menu()
+    if division_number == 12: fxyz_business_menu()
+
+    print(f'Division {division_number} is not a valid entry.  Please try again.')
+
+    return None
+
+def fxyz_organization_menu() -> None:
+
+    return None
+
+def fxyz_physical_menu() -> None:
+
+    return None
+
+def fxyz_mental_menu() -> None:
+
+    return None
+
+def fxyz_spiritual_menu() -> None:
+
+    return None
+
+def fxyz_chronological_menu() -> None:
+
+    return None
+
+def fxyz_financial_menu() -> None:
+
+    return None
+
+def fxyz_material_menu() -> None:
+
+    return None
+
+def fxyz_knowledge_menu() -> None:
+
+    return None
+
+def fxyz_skillsets_menu() -> None:
+
+    return None
+
+def fxyz_creative_menu() -> None:
+
+    return None
+
+def fxyz_social_menu() -> None:
+
+    return None
+
+def fxyz_business_menu() -> None:
+
+    return None
